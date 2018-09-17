ebaadd5287cb383b39d40be791b5e84c080736f4
devBADSI <arathore@badsi.ro>
2018-09-17 15:58:40 +0300

    updated second page buttons using JS

--- Js/appJS.js
+++ Js/appJS.js
@@ -2,3 +2,18 @@ function nextPage(currentPage, secondPage) {
    document.getElementById(currentPage).style.display = "none";
    document.getElementById(secondPage).style.display = "block";
 }
+
+
+function assignValue(event,objectiveValue, objectiveName){
+        var callingButtonID = event.target.id;
+        var x=document.getElementById(objectiveName).innerHTML;
+        console.log(x+"Ajay");
+        if(x == ""){
+            document.getElementById(objectiveName).innerHTML = "<input type='hidden' name="+"'"+objectiveValue+"' "+"value="+"'"+objectiveValue+"'"+">";
+            document.getElementById(callingButtonID).style.backgroundColor= "green";
+        } else {
+            document.getElementById(callingButtonID).style.backgroundColor= "#EEEEEE";
+            document.getElementById(objectiveName).innerHTML = "";
+        }
+    }
+    
